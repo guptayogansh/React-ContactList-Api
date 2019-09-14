1f54071ac4808b166a577a32a72af82e147144b7
guptayogansh <yogansh.11@gmail.com>
2019-09-15 00:58:03 +0530

    Added Contact component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Contacts from './Components/contacts';
 
-function App() {
-  return (
-    <div>
-    </div>
-  );
+
+class App extends Component {
+  
+  state = {
+    contacts: []
+  }
+
+  componentDidMount() {
+    fetch('http://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then((data) => {
+        this.setState({ contacts: data })
+      })
+      .catch(console.log)
+  }
+
+  render() {
+    return (
+      <Contacts contacts={this.state.contacts} />
+    );
+  }
 }
 
 export default App;
